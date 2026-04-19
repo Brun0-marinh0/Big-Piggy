2f57d495d31483f484bacbb4eab5099024ef8f7f
Brun0-marinh0 <85966302+Brun0-marinh0@users.noreply.github.com>
2026-04-19 19:02:27 +0000

    feat: add `color root`

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,18 @@
+import { colors } from "@/src/styles/colors";
 import { Slot } from "expo-router";
+import { StyleSheet, View } from "react-native";
 
 export default function Layout() {
-  return <Slot />;
+  return (
+    <View style={styles.container}>
+      <Slot />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.background,
+  },
+});
